88a9b8879247b7ab0bc0878d51df49a7e742581f
thanduli <104521024+thanduli@users.noreply.github.com>
2025-01-20 20:14:38 +0100

    additional testcases

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -1,17 +1,21 @@
 from companies.models import Company
 import json
 from unittest import TestCase
-import django
 from django.urls import reverse
 from django.test import Client
+import logging
 
 import pytest
+logger = logging.getLogger("log")
 
 
 @pytest.mark.django_db
 class TestGetCompanies(TestCase):
     def test_simple_test(self) -> None:
         pass
+
+    def stuff():
+        print("stuff")
 
     def test_zero_companies_should_return_empty_list(self) -> None:
         client = Client()
@@ -32,3 +36,38 @@ class TestGetCompanies(TestCase):
         self.assertEqual(response_content.get("application_link"), "")
         self.assertEqual(response_content.get("notes"), "")
         testCompany.delete()
+
+
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+
+def function_that_logs() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def function_that_logs_info() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.info(f"I am logging {str(e)}")
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_warning_level_info(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        function_that_logs_info()
+        assert "I am logging CoronaVirus Exception" in caplog.text
